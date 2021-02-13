206b98dfeb548b93c88a53cda71a500444c273e1
GuardianWorld <emanuelvitorsam@gmail.com>
2021-02-13 00:48:36 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,50 @@
+#include "headers/ConsoleInterface.h"
+#include "headers/LoaderConfig.h"
+#include "headers/LoadPasteModule.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x = -1;
+	FILE* configFILE;
+	TypeConfig configuration;
+	openConfigFile(&configuration, &configFILE);
+	while (1)
+	{
+		chooseMenu(&configuration);
+		//printf("%s\n", configuration.path1);
+		scanf("%d", &x);
+		if (x == 1)
+		{
+			//Check mod folder for now
+            ListModInsert(&configuration, configuration.TwoPath);
+		}
+		if (x == 2)
+		{
+            ListModRemoval(&configuration, configuration.TwoPath);
+		}
+		if(x == 3)
+        {
+            printf("*Headpats*\n");
+        }
+        if(x == 4)
+        {
+            printf("Boop :3\n");
+        }
+            if (x == 404)
+        {
+            printf("Made by: CAT (EE)\n");
+        }
+		if (x == 9)
+		{
+			ConfigMenu(&configuration, &configFILE);
+		}
+		if (x == 0)
+        {
+            break;
+        }
+		getchar();
+	}
+	return 0;
+}
